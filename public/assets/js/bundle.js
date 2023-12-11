a283570ef1a0cd026e02b470d34998b0521252bd
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2023-12-11 13:49:25 -0300

    feat: model maked

---
+++ public/assets/js/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var msg = "Hello World";
+document.body.innerHTML = "<h1>".concat(msg, "</h1>");
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
